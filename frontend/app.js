69f16440d3c4a93e11b64370208cc5fdf6a57d39
Kevin Clarens <k.clarens@adveez.com>
2016-04-21 22:09:55 +0200

    feat(font-awesome): move font-awesom import into style folder

--- frontend/app.js
+++ frontend/app.js
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app.react'
 
-//import font-awesome
-import '../node_modules/font-awesome/css/font-awesome.min.css'
-
 // import app style
 import './styles/app.less'
 
